ff27649cd490c9956c4f398b51072357b9d21419
Jean Estevez <ctarriba9@gmail.com>
2023-06-06 12:35:56 -0400

    002 py

--- python/Basic/002-toDoList.py
+++ python/Basic/002-toDoList.py
@@ -34,20 +34,56 @@ def clear_terminal():
     else:  # Unix/Linux/Mac
         os.system('clear')
 
+def update_screen_task():
+    numb_index = 1
+    for task, state in data_today:
+        state_task = " " if state == False else "1"  
+        print(f"{numb_index} - [{state_task}]  {task}")
+        numb_index += 1
+
+def get_task_for_index(n):
+    numb_index = 1
+    task = ""
+    for task, state in data_today:
+        if numb_index == n:
+            return task
+        else:
+            numb_index += 1
+
+
 def new_toDo_list(name):
-    clear_terminal()
-    print(f"\t\tToday: {current_date}")
-    print("[ ] ...")
-    print("\t1-Add\t2-modify\t3-delete")
-    choice = input(">> ")
-    if choice == "1":
-        pass
-    elif choice == "2":
-        pass
-    elif choice == "3":
-        pass
-    else:
-        pass
+    while True:
+        clear_terminal()
+        print(f"\t\tToday: {current_date}")
+        update_screen_task()
+        print("\t1-Add\t2-modify\t3-delete\t4-exit")
+        choice = input(">> ")
+        if choice == "1":
+            description_task = input("task: ")
+            data_today[description_task] = False}
+            continue
+        elif choice == "2":
+            number_task_to_modifiy = int(input("Insert (number) of the task to be modified: "))
+            print("""1-Mark (completed, not completed)
+            2-Modify text""")
+
+            task_modify = get_task_for_index()
+            option_to_modify = int(input(">> "))
+            if option_to_modify == 1:
+                state = data_today[task_modify]
+                data_today[number_task_to_modifiy]["state"] = not state
+                continue
+            elif option_to_modify == 2:
+                new_text = input("New text: ")
+                data_today[number_task_to_modifiy]["text"] = new_text
+                continue
+        elif choice == "3":
+            number_task_to_delete = int(input("Insert (number) of the task to be deleted: "))
+
+        elif choice == "4":
+            pass
+        else:
+            pass
     
 
 # code
