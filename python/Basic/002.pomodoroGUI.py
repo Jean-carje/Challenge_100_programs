f020b6fef16c3783c63d8918bec7b4350b065733
Jean Estevez <ctarriba9@gmail.com>
2024-04-26 13:25:02 -0400

    STUDY

--- python/Basic/002.pomodoroGUI.py
+++ python/Basic/002.pomodoroGUI.py
@@ -7,52 +7,63 @@
 
 
 import time
+import tkinter
 
-# Valores predeterminados
-MIN_POMODORO = 25
-SEC_POMODORO = 0
-MIN_REST = 5
-SEC_REST = 0
+class App:
+    def __init__(self, root) -> None:
+        self.root = root
+        root.title("Pomodoro")
 
-def show_menu():
-    """Menu options."""
-    print("""
-          Select Option:
-          1 - Start
-          """)
+        self.MIN_POMODORO = 0
+        self.SEC_POMODORO = "03"
+        self.MIN_REST = 0
+        self.SEC_REST = "04"
 
-def start_timer(minutes, seconds):
-    """Start Pomodoro and print minutes and seconds."""
-    while minutes > 0 or seconds > 0:
-        print(f"{minutes}:{seconds}")
+        self.show_window()
+
+    def show_window(self):
+        self.lbl = tkinter.Label(root, bg="green", font=("Helvetica", 48), text=f"{str(self.MIN_POMODORO)}:{str(self.SEC_POMODORO)}")
+        self.lbl.pack()
+
+        self.btn_start = tkinter.Button(root, text="Start", command=self.start_timer)
+        self.btn_start.pack()
+
+        self.btn_stop = tkinter.Button(root, text="Stop", command=self.stop_timer)
+        self.btn_stop.pack()
+
+        self.btn_reset = tkinter.Button(root, text="Reset", command=self.reset_timer)
+        self.btn_reset.pack()
+
+    def start_timer(self):
+        if self.MIN_POMODORO > -1:
+            self.lbl.config(text=f"{str(self.MIN_POMODORO)}:{str(self.SEC_POMODORO)}")
+            self.root.after(1000, self.update_timer, self.MIN_POMODORO, int(self.SEC_POMODORO))
+            # self.MIN_POMODORO, self.SEC_POMODORO = -1, 0
+        self.lbl.config(text=f"{str(self.MIN_REST)}:{str(self.SEC_REST)}")
+        self.root.after(1000, self.update_timer, self.MIN_REST, int(self.SEC_REST))
+        # self.MIN_POMODORO, self.SEC_POMODORO = 25, "00"
+
+    def update_timer(self, minutes, seconds):
         if seconds == 0 and minutes > 0:
             minutes -= 1
             seconds = 59
         else:
             seconds -= 1
-            time.sleep(1)
+        self.lbl.config(text=f"{str(minutes)}:{str(seconds)}")
+        if minutes == 0 and seconds == 0:
+            return None
+        self.root.after(1000, self.update_timer, minutes, seconds)
 
-# def reset_values():
-#     """Reset values"""
-#     return MIN_POMODORO, SEC_POMODORO, MIN_REST, SEC_REST
+    def stop_timer(self):
+        pass
 
-def main():
-    """Main funtion."""
+    def reset_timer(self):
+        pass
 
-    while True:
-        show_menu()
-        option = input(" ---> ")
 
-        # if option == '2':
-        #     MIN_POMODORO, SEC_POMODORO, MIN_REST, SEC_REST = reset_values()
-        if option == '1':
-            # Start Pomodoro
-            start_timer(MIN_POMODORO, SEC_POMODORO)
-            # rest time
-            start_timer(MIN_REST, SEC_REST)
-            # reset values
-            # MIN_POMODORO, SEC_POMODORO, MIN_REST, SEC_REST = reset_values()
 
 if __name__ == "__main__":
-    main()
+    root = tkinter.Tk()
+    main = App(root)
+    root.mainloop()
 
