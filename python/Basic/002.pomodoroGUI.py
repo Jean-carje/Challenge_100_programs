788a1d8684971408b046ae38ca3b73ed140b93d7
Jean Estevez <ctarriba9@gmail.com>
2024-05-01 22:26:26 -0400

    project

--- python/Basic/002.pomodoroGUI.py
+++ python/Basic/002.pomodoroGUI.py
@@ -6,7 +6,6 @@
 # System requirements: Windows, Mac, or Linux with Python 3.12 installed
 
 
-import time
 import tkinter
 
 class App:
@@ -14,52 +13,37 @@ class App:
         self.root = root
         root.title("Pomodoro")
 
+        # timers
         self.MIN_POMODORO = 0
         self.SEC_POMODORO = "03"
         self.MIN_REST = 0
-        self.SEC_REST = "04"
+        self.SEC_REST = "06"
 
         self.show_window()
 
     def show_window(self):
-        self.lbl = tkinter.Label(root, bg="green", font=("Helvetica", 48), text=f"{str(self.MIN_POMODORO)}:{str(self.SEC_POMODORO)}")
-        self.lbl.pack()
+        # timer in window
+        self.label = tkinter.Label(self.root, text=f'{self.MIN_POMODORO}:{self.SEC_POMODORO}', bg="green", font=("Helvetica", 42))
+        self.label.pack()
 
-        self.btn_start = tkinter.Button(root, text="Start", command=self.start_timer)
-        self.btn_start.pack()
+        # buttons 
+        self.btn1 = tkinter.Button(self.root, text="Play", command=self.start_timer, font=("Helvetica", 12))
+        self.btn1.pack(side="left", padx=5)
 
-        self.btn_stop = tkinter.Button(root, text="Stop", command=self.stop_timer)
-        self.btn_stop.pack()
-
-        self.btn_reset = tkinter.Button(root, text="Reset", command=self.reset_timer)
-        self.btn_reset.pack()
+        self.btn2 = tkinter.Button(self.root, text="Stop", command=self.stop_timer, font=("Helvetica", 12))
+        self.btn2.pack(side="left", padx=5)
+        
+        self.btn3 = tkinter.Button(self.root, text="Reset", command=self.reset_timer, font=("Helvetica", 12))
+        self.btn3.pack(side="left", padx=5)
 
     def start_timer(self):
-        if self.MIN_POMODORO > -1:
-            self.lbl.config(text=f"{str(self.MIN_POMODORO)}:{str(self.SEC_POMODORO)}")
-            self.root.after(1000, self.update_timer, self.MIN_POMODORO, int(self.SEC_POMODORO))
-            # self.MIN_POMODORO, self.SEC_POMODORO = -1, 0
-        self.lbl.config(text=f"{str(self.MIN_REST)}:{str(self.SEC_REST)}")
-        self.root.after(1000, self.update_timer, self.MIN_REST, int(self.SEC_REST))
-        # self.MIN_POMODORO, self.SEC_POMODORO = 25, "00"
-
-    def update_timer(self, minutes, seconds):
-        if seconds == 0 and minutes > 0:
-            minutes -= 1
-            seconds = 59
-        else:
-            seconds -= 1
-        self.lbl.config(text=f"{str(minutes)}:{str(seconds)}")
-        if minutes == 0 and seconds == 0:
-            return None
-        self.root.after(1000, self.update_timer, minutes, seconds)
+        pass
 
     def stop_timer(self):
         pass
 
     def reset_timer(self):
         pass
-
 
 
 if __name__ == "__main__":
